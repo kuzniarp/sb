6e90be3a840f412b716cf05d860dbb2776276960
kuzniarp <kuzniarp@gmail.com>
2010-01-07 22:33:27 +0100

    updated application layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 require "digest/sha1"
 class User
 	attr_accessor :id, :email, :password 
-	USERS = {'admin1' => {:id => 1, :email => 'admin', :password => '1beac40020e67d7109b160bbbce41ac112c106e0'}, 'admin2' => {:id => 2, :email => 'admin2', :password => '7805d29200c250cf9b2e2550506506e2e1119632'}}
+	USERS = {'admin1' => {:id => 1, :email => 'admin', :password => 'd7d9718a07a7e21ef24adbc823c188cfa801f9ff'}, 'admin2' => {:id => 2, :email => 'admin2', :password => 'd7d9718a07a7e21ef24adbc823c188cfa801f9ff'}}
 	
 	def self.authenticate email, password
 		User::USERS.values.map{|u| {:email => u[:email], :password => u[:password]}}.include?({:email => email, :password => Digest::SHA1.hexdigest(password)})
